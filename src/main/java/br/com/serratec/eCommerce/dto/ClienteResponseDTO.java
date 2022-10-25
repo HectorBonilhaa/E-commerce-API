060eb614a276addc7544070fa9fb738b30ecc985
tavaresmateus <mateustavares123@gmail.com>
2022-10-25 19:41:58 -0300

    Dtos

---
+++ src/main/java/br/com/serratec/eCommerce/dto/ClienteResponseDTO.java
@@ -0,0 +1,95 @@
+package br.com.serratec.eCommerce.dto;
+
+import java.util.Date;
+import java.util.List;
+
+import br.com.serratec.eCommerce.model.Categoria;
+import br.com.serratec.eCommerce.model.Endereco;
+
+
+public class ClienteResponseDTO {
+	
+	private String email;
+	
+	private String nomeUsuario; 
+	
+	private String nomeCompleto;
+	
+	private String senha;
+	
+	private String cpf;
+	
+	private String telefone;
+	
+	private Date dataNascimento;
+
+	private List<EnderecoResponseDTO> enderecos;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public List<EnderecoResponseDTO> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<EnderecoResponseDTO> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+
+
+	
+}
